99ac7ec7f17831a5a01931f2fc2767b1bf57a923
Troels Haugboelle <haugboel@nbi.ku.dk>
2022-12-09 15:15:51 +0100

    ORG compactify and clean up

--- 9/solution.py
+++ 9/solution.py
@@ -1,15 +1,6 @@
 import numpy as np
 
-def move_rope(m,head,tail,move=True):
-    if move:
-        if m=='R':
-            head[0] += 1
-        if m=='L':
-            head[0] -= 1
-        if m=='U':
-            head[1] += 1
-        if m=='D':
-            head[1] -= 1
+def move_rope(head,tail):
     if (abs(head[0]-tail[0]) > 1) or (abs(head[1]-tail[1]) > 1):
         hor_move = False
         ver_move = False
@@ -28,25 +19,20 @@ def move_rope(m,head,tail,move=True):
 
 def move_rope2(m,k):
     knots = np.copy(k)
+    moves = {'R': [1,0],'L': [-1,0],'U': [0,1],'D': [0,-1]}
+    knots[0] += moves[m]
     for i in range(knots.shape[0]-1):
-        head, tail = move_rope(m,knots[i],knots[i+1],move=(i==0))
-        knots[i]   = np.copy(head)
-        knots[i+1] = np.copy(tail)
+        knots[i:i+2] = move_rope(knots[i],knots[i+1])
     return knots
 
 def make_rope(nknots):
     knots = np.zeros((nknots,2))
-    tail_positions = [[0,0]]
+    #tail_positions = [(0,0)]
     with open('data.txt','r') as file:
         for step, l in enumerate(file):
-            move = l[0]
-            times = int(l[1:].strip())
-            for i in range(times):
-                knots = move_rope2(move,knots)
-                tail_positions.append(knots[-1])
-    pos = np.array(tail_positions) # make into a 1D list
-    mx = pos.max()-pos.min()
-    return len(np.unique(pos[:,0] + mx * pos[:,1]))
+            for i in range(int(l[1:].strip())):
+                knots = move_rope2(l[0],knots)
+                tail_positions.append(tuple(knots[-1]))
+    return len(set(tail_positions))
 
-print("1st :", make_rope(2))
-print("2nd :", make_rope(10))
+print("1st :", make_rope(2), "2nd :", make_rope(10))
