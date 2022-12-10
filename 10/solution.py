b12b0c5a63adf3a2ebf587b9fd7ded7f1694d117
Troels Haugboelle <haugboel@nbi.ku.dk>
2022-12-10 08:45:30 +0100

    compactify day 10

--- 10/solution.py
+++ 10/solution.py
@@ -1,6 +1,6 @@
 import numpy as np
-image = np.zeros([40,6],dtype=int)
 with open('data.txt','r') as file:
+    image = ''
     signal = []
     X = 1
     cycle = 0
@@ -8,34 +8,12 @@ with open('data.txt','r') as file:
         cycle += 1
         signal.append(X*cycle)
         instruction = line[0:4]
-        xpos = (cycle-1) % 40
-        ypos = (cycle-1) // 40
-        image[xpos,ypos] = abs(xpos-X)<=1
-        if cycle < 20:
-            print('Cycle: {:0}, instruction {:1}, value: {:2} signal: {:3}'.format( \
-                    cycle,line[0:4],X,signal[-1]))
+        image += {False:' ', True:'#'}[abs((cycle-1) % 40-X)<=1]
         if instruction=='addx':
             cycle += 1
             signal.append(X*cycle)
-            xpos = (cycle-1) % 40
-            ypos = (cycle-1) // 40
-            image[xpos,ypos] = abs(xpos-X)<=1
-            Y = int(line[4:].strip())
-            if cycle < 20:
-                print('Cycle: {:0}, instruction {:1}, value: {:2} signal: {:3} reg: {:4}'.format( \
-                        cycle,line[0:4],X,signal[-1],Y))
-            X = X + Y
-# Find the signal strength during the 
-# 20th, 60th, 100th, 140th, 180th, and 220th cycles. 
-# What is the sum of these six signal strengths?
-signal = np.array(signal)
-# 12760 too low
-print(np.sum(signal[[19,59,99,139,179,219]]))
-for y in range(6):
-    line = ''
-    for x in range(40):
-        if image[x,y]==0:
-            line += ' '
-        else:
-            line += '#'
-    print(line)
+            image += {False:' ', True:'#'}[abs((cycle-1) % 40-X)<=1]
+            X = X + int(line[4:].strip())
+print(np.sum(np.array(signal)[[19,59,99,139,179,219]]))
+for i in range(0, len(image), 40):
+    print(image[i:i+40])
