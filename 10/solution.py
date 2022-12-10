31d731370ba4c2713812cff9e402ddefb69202c8
Troels Haugboelle <haugboel@nbi.ku.dk>
2022-12-10 09:05:38 +0100

    ORG test first part inline

--- 10/solution.py
+++ 10/solution.py
@@ -1,19 +1,16 @@
-import numpy as np
 with open('data.txt','r') as file:
     image = ''
-    signal = []
+    first = 0
     X = 1
     cycle = 0
     for line in file:
         cycle += 1
-        signal.append(X*cycle)
-        instruction = line[0:4]
-        image += {False:' ', True:'#'}[abs((cycle-1) % 40-X)<=1]
-        if instruction=='addx':
+        if cycle in (20,60,100,140,180,220): first += X*cycle
+        image += {False:' ', True:'#'}[abs(((cycle-1) % 40) - X)<=1]
+        if line[0:4]=='addx':
             cycle += 1
-            signal.append(X*cycle)
-            image += {False:' ', True:'#'}[abs((cycle-1) % 40-X)<=1]
+            if cycle in (20,60,100,140,180,220): first += X*cycle
+            image += {False:' ', True:'#'}[abs(((cycle-1) % 40) - X)<=1]
             X = X + int(line[4:].strip())
-print(np.sum(np.array(signal)[[19,59,99,139,179,219]]))
-for i in range(0, len(image), 40):
-    print(image[i:i+40])
+print('1st part: ', first)
+for i in range(0, len(image), 40): print(image[i:i+40])
